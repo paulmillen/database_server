0199c429f4b6f6e9f8e74e8bd774da5a1954cbbb
paulmillen <pwmillen@gmail.com>
2017-09-04 16:58:22 +0100

    removed controller before function

--- app/controllers/memory.rb
+++ app/controllers/memory.rb
@@ -1,15 +1,11 @@
 class App < Sinatra::Base
 
-  before do
-    @memory = Memory.instance
-  end
-
   get '/set' do
     Memory.create(request.params)
   end
 
   get '/get' do
-    @memory.return_value(request.params)
+    Memory.instance.return_value(request.params)
   end
 
 end
